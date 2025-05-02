5725d7abbb67e3b4c5935931359b5590f62b2cf8
Lenovo <Lenovo@DESKTOP-B9FB00R>
2025-05-02 15:11:45 +0330

    [feat] add swagger

--- Planology.Authentication/API/Controllers/AclController.cs
+++ Planology.Authentication/API/Controllers/AclController.cs
@@ -1,11 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Requests.Login;
+using Application.UseCases.ACL;
+using Application.UseCases.Login;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AclController : ControllerBase
+    public class ACLController : ControllerBase
     {
+        private readonly CheckUserHasPermissionOnObjectUseCase _checkUserHasPermissionOnObjectUseCase;
+        private readonly AssignPermissionToUserOnObjectUseCase _assignPermissionToUserOnObjectUseCase;
+        private readonly LoginUserUseCase _loginUserHandler;
+
+        public ACLController(
+            CheckUserHasPermissionOnObjectUseCase checkUserHasPermissionOnObjectUseCase,
+            AssignPermissionToUserOnObjectUseCase assignPermissionToUserOnObjectUseCase)
+        {
+            _checkUserHasPermissionOnObjectUseCase = checkUserHasPermissionOnObjectUseCase;
+            _assignPermissionToUserOnObjectUseCase = assignPermissionToUserOnObjectUseCase;
+        }
+
+        [HttpPost("CheckUserHasPermission")]
+        public async Task<IActionResult> CheckUserHasPermission(RegisterUserRequest request)
+        {
+            var result = await _checkUserHasPermissionOnObjectUseCase.ExecuteAsync(request);
+            return Ok(result);
+        }
+
+        [HttpPost("AssignPermissionToUser")]
+        public async Task<IActionResult> AssignPermissionToUser(LoginUserRequest request)
+        {
+            var result = await _assignPermissionToUserOnObjectUseCase.ExecuteAsync(request);
+            return Ok(result);
+        }
 
     }
 }
