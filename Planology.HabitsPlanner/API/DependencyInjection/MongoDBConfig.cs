5bd0e0f356c2e93b502a431b935e84804902fd88
Ghazaleh7979 <masoome.aliyannejhadi@gmail.com>
2025-05-10 23:14:27 +0330

    [feat] add mongoDb Config

---
+++ Planology.HabitsPlanner/API/DependencyInjection/MongoDBConfig.cs
@@ -0,0 +1,30 @@
+﻿using Infrastructure.Database;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace API.DependencyInjection
+{
+    public static class MongoDBConfig
+    {
+        public static IServiceCollection AddMongoDB(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.Configure<MongoDbSettings>(
+            configuration.GetSection("MongoDbSettings"));
+
+            services.AddSingleton<IMongoClient>(sp =>
+            {
+                var settings = sp.GetRequiredService<IOptions<MongoDbSettings>>().Value;
+                return new MongoClient(settings.ConnectionString);
+            });
+
+            services.AddScoped(sp =>
+            {
+                var settings = sp.GetRequiredService<IOptions<MongoDbSettings>>().Value;
+                var client = sp.GetRequiredService<IMongoClient>();
+                return client.GetDatabase(settings.DatabaseName);
+            });
+            return services;
+        }
+    }
+}
