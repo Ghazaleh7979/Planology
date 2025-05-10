3a9d5d681737c0b605fb9a083aa6287c5e53854f
Lenovo <Lenovo@DESKTOP-B9FB00R>
2025-05-10 22:39:33 +0330

    [feat] Template

---
+++ Planology.HabitsPlanner/API/DependencyInjection/RepositoryConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace API.DependencyInjection
+{
+    public static class RepositoryConfig
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+
+
+            return services;
+        }
+    }
+}
---
+++ Planology.HabitsPlanner/API/Middlewares/MiddlewareExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Middlewares
+{
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseGlobalExceptionHandler(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<ExceptionHandlingMiddleware>();
+        }
+    }
+}
