7ba34f2cd6cc8f98489cf2e9173a89c37e1426d4
Aliyannejadi <masoome.aliyannejhadi@gmail.com>
2025-05-11 15:00:04 +0330

    [fix] inject repositories in program.cs

--- Planology.HabitsPlanner/API/DependencyInjection/RepositoryConfig.cs
+++ Planology.HabitsPlanner/API/DependencyInjection/RepositoryConfig.cs
@@ -1,10 +1,15 @@
-﻿namespace API.DependencyInjection
+﻿using Domain.IRepository;
+using Infrastructure.Repositories;
+
+namespace API.DependencyInjection
 {
     public static class RepositoryConfig
     {
         public static IServiceCollection AddRepositories(this IServiceCollection services)
         {
-
+            services.AddScoped<IHabitRepository, HabitRepository>();
+            services.AddScoped<IGoalRepository, GoalRepository>();
+            services.AddScoped<IMeasurementUnitRepository, MeasurementUnitRepository>();
 
             return services;
         }
