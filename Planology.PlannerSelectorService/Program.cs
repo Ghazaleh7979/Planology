4d7301118ccdc27895f1d244d2c5dbce0cea3770
Ghazaleh7979 <masoome.aliyannejhadi@gmail.com>
2025-05-21 17:47:23 +0330

    [fix] connection to redis

--- Planology.PlannerSelectorService/Program.cs
+++ Planology.PlannerSelectorService/Program.cs
@@ -1,12 +1,31 @@
+using Microsoft.IdentityModel.Tokens;
 using Planology.PlannerSelectorService.Features.GetAvailablePlanners;
 using Planology.PlannerSelectorService.Features.SelectPlanner;
 using StackExchange.Redis;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+            ValidAudience = builder.Configuration["JwtSettings:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:SecretKey"]))
+        };
+    });
+
 builder.Services.AddSingleton<IConnectionMultiplexer>(
     ConnectionMultiplexer.Connect(builder.Configuration.GetConnectionString("Redis") ?? "localhost"));
 
@@ -23,6 +42,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
